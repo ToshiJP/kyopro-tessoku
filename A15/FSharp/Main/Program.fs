6a32eeea3714fc74f2b7b06ec1d893fb4afab5cc
Toshi <t0324s@gmail.com>
2023-01-13 00:58:55 +0900

    add codes

---
+++ A15/FSharp/Main/Program.fs
@@ -0,0 +1,22 @@
+﻿let N = stdin.ReadLine() |> int
+let A = stdin.ReadLine().Trim().Split() |> Array.map int
+
+let T = A |> Set.ofArray |> Set.toArray |> Array.sort
+
+let binarySearch x (ary : int[]) =
+    let rec binarySearchSub ng ok =
+        if ok - ng > 1 then
+            let mid = (ng + ok) / 2
+            if ary.[mid] >= x then
+                binarySearchSub ng mid
+            else
+                binarySearchSub mid ok
+        else
+            ok
+    binarySearchSub -1 ary.Length
+
+let B = Array.map (fun a -> binarySearch a T + 1) A
+
+printf "%d" B.[0]
+for i = 1 to N - 1 do
+    printf " %d" B.[i]
